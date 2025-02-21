c9696dc3c4dc975d1c6c241f8bb602bbc6e20579
G-bolanos <bolanosgabriela60@gmail.com>
2025-02-21 00:04:33 -0600

    test_1

--- catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import create_user
-
-urlpatterns = [
-    path("", views.home_page, name="home_page"),
-    path("create", views.create_user, name="create_user"),
-    path("update", views.update_user, name="update_user"),
-]
